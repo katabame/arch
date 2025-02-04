1109053c8390a7355d798ea419c6a3f6f0e7d974
本田珠彩 <ayame@kataba.me>
2025-02-04 13:48:02 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,20 @@
-# yay
 sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git
-cd yay
+cd ./yay
 makepkg -si
 cd ../
+rm -rf ./yay
 
-yay -S google-chrome
-yay -S fcitx5 fcitx5-mozc noto-fonts-cjk
+yay -S google-chrome fcitx5-im fcitx5-mozc noto-fonts-cjk noto-fonts-emoji \
+       discord 1password alacritty waybar github-cli fuzzel steam unzip \
+       ttf-font-awesome pavucontrol lunar-client xivlauncher cider2-bin thunderbird \
+       obs-studio virtualbox cursor-bin prismlauncher-qt5-bin swaync wireplumber xremap-hypr-bin
+       
+
+# xremap
+# wget -O xremap.zip https://github.com/xremap/xremap/releases/latest/download/xremap-linux-x86_64-hypr.zip
+# unzip ./xremap.zip
+# sudo mv ./xremap /usr/local/bin
+echo 'uinput' | sudo tee /etc/modules-load.d/uinput.conf
+echo 'KERNEL=="uinput", GROUP="input", TAG+="uaccess"' | sudo tee /etc/udev/rules.d/99-input.rules
+# rm ./xremap.zip
